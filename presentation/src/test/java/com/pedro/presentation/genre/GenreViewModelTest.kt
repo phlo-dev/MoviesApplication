a08d43799dac264c9efcbe08474782c8766c3f0e
ordep-oliveira <pedrooliveira@ioasys.com.br>
2020-08-08 13:03:42 -0300

    refactoring unit tests after change view model logic of GenreViewModel

--- presentation/src/test/java/com/pedro/presentation/genre/GenreViewModelTest.kt
+++ presentation/src/test/java/com/pedro/presentation/genre/GenreViewModelTest.kt
@@ -1,10 +1,13 @@
 package com.pedro.presentation.genre
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.pedro.domain.models.GenreTypeParamEnum
+import com.pedro.domain.models.ListPagination
 import com.pedro.domain.models.Movie
 import com.pedro.domain.usecases.GetMovies
 import com.pedro.presentation.ViewState
 import com.pedro.presentation.ViewState.Status.*
+import com.pedro.presentation.mapper.fromDomain
 import com.pedro.presentation.models.GenreTypeEnum
 import io.mockk.every
 import io.mockk.invoke
@@ -18,6 +21,8 @@ import org.koin.dsl.module.module
 import org.koin.standalone.StandAloneContext.startKoin
 import org.koin.standalone.StandAloneContext.stopKoin
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class GenreViewModelTest {
     @get:Rule
@@ -51,10 +56,12 @@ class GenreViewModelTest {
 
     @Test
     fun `fetchMovieListByGenre WHEN has a success from getMovies use case MUST post a list of movies and success status`() {
+        val movieList = listOf(Movie(1, "", "", ""))
         val expectedValue = ViewState(
-            status = SUCCESS, data = listOf<com.pedro.presentation.models.Movie>()
+            status = SUCCESS,
+            data = movieList.fromDomain()
         )
-        stubSuccessInGetMovies()
+        stubSuccessInGetMovies(ListPagination(movieList))
         viewModel.fetchMovieListByGenre()
         verify(exactly = 1) { getMovies.execute(any(), any(), any()) }
         assertEquals(expectedValue, viewModel.getGenreMoviesViewState().value)
@@ -70,60 +77,48 @@ class GenreViewModelTest {
     }
 
     @Test
-    fun `refreshMovieList WHEN has a error from getMovies MUST post that error`() {
-        val exception = IllegalAccessException()
-        stubErrorInGetMoviesRefresh(exception)
-        viewModel.refreshMovieList()
-        verify(exactly = 1) { getMovies.refresh(any(), any(), any()) }
-        assertEquals(
-            ViewState(status = ERROR, throwable = exception),
-            viewModel.getGenreMoviesViewState().value
-        )
-    }
-
-    @Test
-    fun `refreshMovieList WHEN has a success from getMovies use case MUST post a list of movies and success status`() {
-        val expectedValue = ViewState(
-            status = SUCCESS, data = listOf<com.pedro.presentation.models.Movie>()
-        )
-        stubSuccessInGetMoviesRefresh()
-        viewModel.refreshMovieList()
-        verify(exactly = 1) { getMovies.refresh(any(), any(), any()) }
-        assertEquals(expectedValue, viewModel.getGenreMoviesViewState().value)
-    }
-
-    @Test
-    fun `refreshMovieList WHEN has call when viewState is loading MUST maintain in loading and cancel use case`() {
-        every { getMovies.refresh(any(), any(), any()) } returns Unit
+    fun `refreshMovieList WHEN viewState is loading MUST cancel use case and call him again`() {
+        every { getMovies.execute(any(), any(), any()) } returns Unit
         every { getMovies.cancel() } returns Unit
+        viewModel.fetchMovieListByGenre()
         viewModel.refreshMovieList()
-        viewModel.refreshMovieList()
-        verify(exactly = 2) { getMovies.refresh(any(), any(), any()) }
         verify(exactly = 1) { getMovies.cancel() }
-        assertEquals(LOADING, viewModel.getGenreMoviesViewState().value?.status)
+        verify(exactly = 2) { getMovies.execute(any(), any(), any()) }
     }
 
-    private fun stubSuccessInGetMoviesRefresh() {
-        every {
-            getMovies.refresh(any(), onSuccess = captureLambda(), onFailure = any())
-        } answers {
-            lambda<(List<Movie>) -> Unit>().invoke(listOf())
-        }
+    @Test
+    fun `refreshMovieList WHEN viewState is not loading MUST call getMovies useCase with param page 1`() {
+        val expectedParam = GetMovies.Param(GenreTypeParamEnum.ACTION, 1)
+        stubSuccessInGetMovies()
+        viewModel.refreshMovieList()
+        verify(exactly = 1) { getMovies.execute(expectedParam, any(), any()) }
     }
 
-    private fun stubErrorInGetMoviesRefresh(exception: IllegalAccessException) {
-        every {
-            getMovies.refresh(any(), any(), captureLambda())
-        } answers {
-            lambda<(Throwable) -> Unit>().invoke(exception)
-        }
+    @Test
+    fun `hasMoreResults WHEN called after execute getMovies with success and total page answer is 1 UseCase MUST return false`() {
+        stubSuccessInGetMovies(ListPagination(listOf(), totalPage = 1))
+        viewModel.fetchMovieListByGenre()
+        assertFalse(viewModel.hasMoreResults())
     }
 
-    private fun stubSuccessInGetMovies() {
+    @Test
+    fun `hasMoreResults WHEN call getMovies once success with total page 3 MUST return value true`() {
+        stubSuccessInGetMovies(ListPagination(listOf(), totalPage = 3))
+        viewModel.fetchMovieListByGenre()
+        assertTrue(viewModel.hasMoreResults())
+    }
+
+    @Test
+    fun `hasMoreResults WHEN has not call once getMovies useCase with success MUST return true`() {
+        assertTrue(viewModel.hasMoreResults())
+    }
+
+
+    private fun stubSuccessInGetMovies(data: ListPagination<Movie> = ListPagination(listOf())) {
         every {
             getMovies.execute(any(), onSuccess = captureLambda(), onFailure = any())
         } answers {
-            lambda<(List<Movie>) -> Unit>().invoke(listOf())
+            lambda<(ListPagination<Movie>) -> Unit>().invoke(data)
         }
     }
 
