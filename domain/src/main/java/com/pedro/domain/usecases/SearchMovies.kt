45554cdf7e243b4f8f86e388995ff931ef72e80f
ordep-oliveira <pedrooliveira@ioasys.com.br>
2020-08-09 15:44:37 -0300

    removing complexity from search Movies use Case and passing the responsibility of pagination to viewModel

--- domain/src/main/java/com/pedro/domain/usecases/SearchMovies.kt
+++ domain/src/main/java/com/pedro/domain/usecases/SearchMovies.kt
@@ -2,48 +2,22 @@ package com.pedro.domain.usecases
 
 import com.pedro.domain.models.ListPagination
 import com.pedro.domain.models.Movie
-import com.pedro.domain.models.Response
 import com.pedro.domain.repository.MoviesRepository
 import kotlinx.coroutines.CoroutineScope
 
 class SearchMovies(
     scope: CoroutineScope,
     private val repository: MoviesRepository
-) : UseCase<String, List<Movie>>(scope) {
-    private val movieList = mutableListOf<Movie>()
-    private var query = ""
-    private var currentPage = 1
-    private var totalPage = 1
+) : UseCase<SearchMovies.Param, ListPagination<Movie>>(scope) {
 
-    fun hasMoreResults() = currentPage <= totalPage
+    override suspend fun getResult(param: Param) = repository.searchMoviesWithQuery(
+        query = param.query,
+        page = param.page
+    )
 
-    private suspend fun Response<ListPagination<Movie>>.convertToPresentation() = map {
-        incrementingPagination(it)
-        movieList
-    }
+    data class Param(
+        val query: String,
+        val page: Int
+    )
 
-    private fun incrementingPagination(it: ListPagination<Movie>) {
-        movieList.addAll(it.list)
-        currentPage++
-        totalPage = it.totalPage
-    }
-
-    override suspend fun getResult(param: String): Response<List<Movie>> {
-        checkIfIsRefresh(param)
-        return when (hasMoreResults() && param.isNotBlank()) {
-            true -> repository.searchMoviesWithQuery(
-                query = param,
-                page = currentPage
-            ).convertToPresentation()
-            false -> Response.Success(listOf())
-        }
-    }
-
-    private fun checkIfIsRefresh(param: String) {
-        if(param == query) return
-        query = param
-        currentPage = 1
-        totalPage = 1
-        movieList.clear()
-    }
 }
--- presentation/src/main/java/com/pedro/presentation/search/SearchViewModel.kt
+++ presentation/src/main/java/com/pedro/presentation/search/SearchViewModel.kt
@@ -11,20 +11,39 @@ import org.koin.standalone.KoinComponent
 class SearchViewModel : ViewModel(), KoinComponent {
     private val queryViewState = createViewState<List<Movie>>()
     private val searchMovies: SearchMovies by useCase()
+    private val movieList = mutableListOf<Movie>()
     private var searchQuery = ""
+    private var currentPage = 1
+    private var totalPage = 1
 
     fun search(query: String = searchQuery) {
-        if (queryViewState.value?.isLoading() == true) searchMovies.cancel()
-        searchQuery = query
+        when {
+            queryViewState.value?.isLoading() == true && searchQuery == query -> return
+            queryViewState.value?.isLoading() == true -> searchMovies.cancel()
+        }
+        refreshListIfNeeded(query)
         queryViewState.value = loading()
         searchMovies.execute(
-            param = query,
-            onSuccess = { queryViewState.postSuccess(it.fromDomain()) },
+            param = SearchMovies.Param(query = query, page = currentPage),
+            onSuccess = {
+                movieList.addAll(it.list.fromDomain())
+                currentPage++
+                totalPage = it.totalPage
+                queryViewState.postSuccess(movieList)
+            },
             onFailure = { queryViewState.postFailure(it) }
         )
     }
 
-    fun hasMoreResults() = searchMovies.hasMoreResults()
+    private fun refreshListIfNeeded(query: String) {
+        if (searchQuery == query) return
+        searchQuery = query
+        currentPage = 1
+        totalPage = 1
+        movieList.clear()
+    }
+
+    fun hasMoreResults() = currentPage <= totalPage
 
     fun getQueryViewState() = queryViewState.asLiveData()
 
