7e3ac28c64761f2094794edb9eecf812ffc10082
ordep-oliveira <pedrooliveira@ioasys.com.br>
2020-08-09 16:58:48 -0300

    adding search view model few unit tests

--- presentation/src/test/java/com/pedro/presentation/search/SearchViewModelTest.kt
+++ presentation/src/test/java/com/pedro/presentation/search/SearchViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.pedro.presentation.search
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.pedro.domain.models.ListPagination
 import com.pedro.domain.models.Movie
 import com.pedro.domain.usecases.SearchMovies
 import com.pedro.presentation.ViewState
@@ -18,6 +19,8 @@ import org.koin.dsl.module.module
 import org.koin.standalone.StandAloneContext.startKoin
 import org.koin.standalone.StandAloneContext.stopKoin
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class SearchViewModelTest {
     @get:Rule
@@ -59,11 +62,65 @@ class SearchViewModelTest {
         )
     }
 
-    private fun stubSuccessInSearchMovies() {
+    @Test
+    fun `search WHEN call and has a search call with different query in loading MUST cancel the first and call the second`() {
+        val firstQuery = "123"
+        val secondQuery = "12434"
+        stubSearchMoviesCall()
+        stubSearchMoviesCancel()
+        viewModel.search(firstQuery)
+        stubSuccessInSearchMovies()
+        viewModel.search(secondQuery)
+        verify(exactly = 1) {
+            searchMovies.execute(SearchMovies.Param(firstQuery, 1), any(), any())
+        }
+        verify(exactly = 1) { searchMovies.cancel() }
+        verify(exactly = 1) {
+            searchMovies.execute(SearchMovies.Param(secondQuery, 1), any(), any())
+        }
+    }
+
+    @Test
+    fun `search WHEN call and has a search call with same query in loading MUST maintain viewState to loading and not call the use case second time`() {
+        stubSearchMoviesCall()
+        viewModel.search()
+        viewModel.search()
+        verify(exactly = 1) { searchMovies.execute(any(), any(), any()) }
+        assertEquals(ViewState.Status.LOADING, viewModel.getQueryViewState().value?.status)
+    }
+
+    @Test
+    fun `hasMoreResults WHEN called after execute getMovies with success and total page answer is 1 UseCase MUST return false`() {
+        stubSuccessInSearchMovies()
+        viewModel.search()
+        assertFalse(viewModel.hasMoreResults())
+    }
+
+    @Test
+    fun `hasMoreResults WHEN call getMovies once success with total page 3 MUST return value true`() {
+        stubSuccessInSearchMovies(ListPagination(listOf(), totalPage = 3))
+        viewModel.search()
+        assertTrue(viewModel.hasMoreResults())
+    }
+
+    @Test
+    fun `hasMoreResults WHEN has not call once getMovies useCase with success MUST return true`() {
+        assertTrue(viewModel.hasMoreResults())
+    }
+
+    private fun stubSearchMoviesCancel() {
+        every { searchMovies.cancel() } returns Unit
+    }
+
+    private fun stubSearchMoviesCall() {
+        every { searchMovies.execute(any(), any(), any()) } returns Unit
+    }
+
+    private fun stubSuccessInSearchMovies(data: ListPagination<Movie> = ListPagination(listOf())) {
         every {
             searchMovies.execute(any(), onSuccess = captureLambda(), onFailure = any())
         } answers {
-            lambda<(List<Movie>) -> Unit>().invoke(listOf())
+            lambda<(ListPagination<Movie>) -> Unit>().invoke(data)
         }
     }
 
